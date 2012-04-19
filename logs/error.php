fe33b7ef25600bcd9df3ae222fc2ad3d5a0bb13f
William <catcat811@gmail.com>
2012-04-19 21:31:36 +0800

    + joomla2.5 setup

---
+++ logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-04-19 13:04:14 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2012-04-19	13:04:14	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-04-19	13:04:25	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-04-19	13:05:00	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-04-19	13:06:37	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-04-19	13:06:45	INFO	127.0.0.1	Joomla CANCELED: 	
